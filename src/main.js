2a0b9f81b959ba9608755a31ab7c853d221b683a
ISHNN <ishnn032n@gmail.com>
2021-08-05 16:41:38 +0900

    feat: Add Vuetify

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router/index";
-
+import vuetify from "@/plugins/vuetify";
 import PaperDashboard from "./plugins/paperDashboard";
 import "vue-notifyjs/themes/default.css";
 
@@ -24,6 +24,7 @@ Vue.use(PaperDashboard);
 
 /* eslint-disable no-new */
 new Vue({
+  vuetify,
   router,
   render: h => h(App)
 }).$mount("#app");
